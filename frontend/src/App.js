3e6e3505210306a5b47269bb837ec82d2e4a75bd
abhishek351 <abhishek291013@gmail.com>
2023-07-31 21:58:35 +0530

    add chatbot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,18 @@
 import { Fragment } from 'react';
-
+import ChatBot from './ChatBot';
 import Header from './components/Layout/Header';
 import Meals from "./components/Meals/Meals"
 function App() {
-  const iframeStyle = {
-    position: 'fixed',
-    bottom: '20px',
-    right: '20px',
-    width: '350px',
-    height: '430px',
-    border: 'none',
-  };
+  
   return (
     <Fragment>
+      
       <Header />
       <main>
         <Meals />
       </main>
-      {/* <iframe
-    allow="microphone;"
-    style={iframeStyle}
-    
-    src="https://console.dialogflow.com/api-client/demo/embedded/21105ea5-d1f1-4242-9478-df6caa55b317">
-</iframe> */}
+    {/* <ChatBot/> */}
+     
     </Fragment>
     
   );
---
+++ frontend/src/ChatBot.js
@@ -0,0 +1,24 @@
+const ChatBot=()=>{
+    const iframeStyle = {
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        width: '300px',
+        height: '400px',
+        border: 'none',
+      };
+    return (
+        <div>
+            <iframe
+    allow="microphone;"
+    style={iframeStyle}
+    
+    src="https://console.dialogflow.com/api-client/demo/embedded/21105ea5-d1f1-4242-9478-df6caa55b317">
+</iframe> 
+        </div>
+        
+    )
+    
+}
+
+export default ChatBot;
